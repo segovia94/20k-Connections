6d0f7531dba12032c6f62e9b317a06227eb552af
Mark Miller <mark@zhukmedia.com>
2018-06-28 17:49:51 -0700

    update packages and nuxt version to 1.4.1

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,8 +1,8 @@
 import { FirebaseApp } from '~/services/firebase'
 
-export default function ({ isServer, req }) {
+export default function ({ req }) {
   // If nuxt generate, pass this middleware
-  if (isServer && !req) return
+  if (process.server && !req) return
 
   FirebaseApp.auth().signInAnonymously().catch(error => {
     console.error('Error Signing In! Code: %o, Message: %o', error.code, error.message)
